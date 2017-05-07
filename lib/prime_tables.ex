edd4bb310085c406d9842bb4130f918bb3b95a66
Sebastien Varlet <sebastien.varlet@gmail.com>
2017-05-07 18:39:37 +0100

    Print table in the console and parse user input

--- lib/prime_tables.ex
+++ lib/prime_tables.ex
@@ -1,18 +1,66 @@
 defmodule PrimeTables do
-  @moduledoc """
-  Documentation for PrimeTables.
-  """
 
-  @doc """
-  Hello world.
+  alias PrimeTables.Generation.PrimeGenerator, as: PrimeGenerator
 
-  ## Examples
+  @column_separator " | "
 
-      iex> PrimeTables.hello
-      :world
+  def main(args) do
+    args
+    |> parse_args
+    |> process
+  end
 
-  """
-  def hello do
-    :world
+  defp parse_args(args) do
+    case OptionParser.parse(args, aliases: [n: :number], strict: [number: :integer]) do
+      {[number: n_primes], [], []} -> {:ok, n_primes}
+      _ -> {:error, :show_usage}
+    end
+  end
+
+  defp process({:error, :show_usage}) do
+    """
+    Usage
+    =====
+    ./prime_table -n [number_of_primes]
+    """
+    |> IO.puts
+  end
+
+  defp process({:ok, n_primes}) do
+    n_primes
+    |> PrimeGenerator.generate
+    |> print_table
+  end
+
+  def print_table({:error, message}) do
+    IO.puts message
+  end
+
+  def print_table({:ok, []}) do
+    IO.puts "No primes were found."
+  end
+
+  def print_table({:ok, primes}) do
+    last_prime = List.last(primes)
+    column_width = String.length("#{last_prime * last_prime}")
+
+    display_row(" ", column_width, primes)
+
+    for p <- primes do
+      display_row("#{p}", column_width, Enum.map(primes, &(&1 * p)))
+    end
+  end
+
+  defp display_row(prefix, desired_length, numbers) do
+    values = Enum.map(numbers, &("#{&1}"))
+    [prefix | values]
+    |> Enum.map(fn item -> pad(item, desired_length) end)
+    |> Enum.join(@column_separator)
+    |> IO.puts
+  end
+
+  defp pad(string, desired_length) do
+    required_spaces = desired_length - String.length(string)
+    String.duplicate(" ", required_spaces) <> string
   end
 end
