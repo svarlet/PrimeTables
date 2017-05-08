4cfa9a7cd62df98684a4245f4e8d49a063a930c3
Sébastien Varlet <sebastien.varlet@gmail.com>
2017-05-08 13:01:41 +0100

    Define a has_composite? function in the Sieve module.

--- lib/prime_tables/generation/prime_generator.ex
+++ lib/prime_tables/generation/prime_generator.ex
@@ -18,28 +18,13 @@ defmodule PrimeTables.Generation.PrimeGenerator do
   end
 
   defp generate(n, primes, candidate, sieve) do
-    if (Map.has_key?(sieve, candidate)) do
-        updated_sieve = update_iterators(sieve, candidate)
+    if (Sieve.has_composite?(sieve, candidate)) do
+        updated_sieve = Sieve.update_iterators(sieve, candidate)
         generate(n, primes, candidate + 1, updated_sieve)
     else
         updated_sieve = Sieve.record_prime(sieve, candidate)
         generate(n - 1, [candidate | primes], candidate + 1, updated_sieve)
     end
-  end
-
-  defp update_iterators(sieve, candidate) do
-    %{^candidate => iterators} = sieve
-    updated_iterators = Enum.map(iterators, &bump_iterator/1)
-    trimmed_sieve = Map.delete(sieve, candidate)
-    Enum.reduce(updated_iterators, trimmed_sieve,
-      fn {composite, _prime} = iterator, sieve ->
-        Map.update(sieve, composite, [iterator], fn iterators -> [iterator | iterators] end)
-      end
-    )
-  end
-
-  defp bump_iterator({composite, prime}) do
-    {composite + prime, prime}
   end
 
 end
